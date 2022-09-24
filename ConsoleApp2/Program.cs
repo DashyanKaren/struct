7f7594ffd817a7160cd383f1896ee8d15283f870
Karen Dashyan <dashyankaren@outlook.com>
2022-09-24 20:48:47 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MyName;
+
+namespace MyName
+{
+    class MyClass
+    {
+        public override string ToString()
+        {
+            return "Hello";
+        }
+    }
+}
+
+namespace ConsoleApp2
+{
+    class Program : Object
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            Console.WriteLine(myClass.GetType());
+            int x = 9;
+            string s = x.ToString();
+            Console.WriteLine(s);
+            Console.WriteLine(myClass.ToString());
+
+        }
+    }
+
+
+}
